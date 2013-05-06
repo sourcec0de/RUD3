5cedb8f378c20961d749eed1d29cabe8174e7287
James R. Qualls <Qualls.James@gmail.com>
2013-05-06 00:20:01 -0700

    Formating bar hieght and spacing

--- BarChart.js
+++ BarChart.js
@@ -1,6 +1,20 @@
 function barChart(data){
 	// select body, append div, with class "bar"
-	d3.select("body").append("div").attr("class", "bar");
+	d3.select("body").selectAll("div")
+	// iterate over data
+	.data(data)
+	// create new element if data.count > dom.elms
+	.enter()
+	// type of elm to be created for each data value
+	.append("div")
+	// class to be added to each elm
+	.attr("class", "bar")
+	// adjust the height of each elm based on the val
+	// passed into the callback
+	// .style("height","25px") -> first val in array d*5 = "5*5+px"
+	.style("height", function(d) {
+		return (d * 5) + "px";
+	});
 
 	// Additioanlly to toggle classes you can use
 	// .classed("bar",true) -> Toggles bar class on elm
