36ea132a40cf6199d3a5720a5cb61d30e13bb902
James R. Qualls <Qualls.James@gmail.com>
2013-05-06 00:12:15 -0700

    working with divs, and making a bar

--- BarChart.js
+++ BarChart.js
@@ -1,30 +1,11 @@
 function barChart(data){
-	// Select body element and all its paragraphs
-	d3.select("body").selectAll("p")
-	// Count all elemnts in the data array
-	.data(data)
-	// look at all dom elements and data values
-	// if data.count > dom.elemnts create new element
-	// to bind the data to
-	.enter()
-	// Create a new paragraph element for everyone that enter returns
-	.append("p")
-	// The text for each element is to be set here
-	// We use a callBack function as the first arg
-	// so that it will return the data value for the
-	// value of the paragraph elements
-	.text(function(d){return "Strings with dynamic numbers "+d;})
-	// Here we can set a style on each elm
-	// we can use a callBack as the second arg here
-	// pass it the data value and use it to set styles
-	// conditionally
-	.style("color",function(d) {
-		if (d > 15) {
-			return "red";
-		} else {
-			return "black";
-		}
-	});
+	// select body, append div, with class "bar"
+	d3.select("body").append("div").attr("class", "bar");
+
+	// Additioanlly to toggle classes you can use
+	// .classed("bar",true) -> Toggles bar class on elm
+	// .classed("bar",false)
+
 }
 function makeChart(){
 	var data = [ 5, 10, 15, 20, 25 ];
