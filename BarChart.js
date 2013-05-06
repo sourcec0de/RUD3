b1f211f8e2b8a5902f1d61fc25b67d3eccb58c56
James R. Qualls <Qualls.James@gmail.com>
2013-05-06 00:00:16 -0700

    Adding styling

--- BarChart.js
+++ BarChart.js
@@ -10,7 +10,12 @@ function barChart(data){
 	// Create a new paragraph element for everyone that enter returns
 	.append("p")
 	// The text for each element is to be set here
-	.text("New paragraph!");
+	// We use a callBack function as the first arg
+	// so that it will return the data value for the
+	// value of the paragraph elements
+	.text(function(d){return "Strings with dynamic numbers "+d;})
+	// Here we can set a style on each elm
+	.style("color","red");
 }
 function makeChart(){
 	var data = [ 5, 10, 15, 20, 25 ];
